674f2d1243fe1a4c41a751d1c25c6ad02648327f
Sergio Ivan Villamizar Alvarado - Ceiba Software <sergio.villamizar@ceiba.com.co>
2019-07-05 10:23:41 -0500

    implementacion de apigateway con zuul y eureka

--- src/main/java/co/com/ceiba/parqueadero/CeibaEstacionamientoApplication.java
+++ src/main/java/co/com/ceiba/parqueadero/CeibaEstacionamientoApplication.java
@@ -1,10 +1,19 @@
 package co.com.ceiba.parqueadero;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Lazy;
+
+import com.netflix.discovery.EurekaClient;
 
 @SpringBootApplication
 public class CeibaEstacionamientoApplication {
+	
+	
+	 @Autowired
+	 @Lazy
+	 private EurekaClient eurekaClient;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CeibaEstacionamientoApplication.class, args);
