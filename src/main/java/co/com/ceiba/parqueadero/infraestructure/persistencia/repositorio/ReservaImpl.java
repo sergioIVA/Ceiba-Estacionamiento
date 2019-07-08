d0ea6519953b6d190a7dea125a31e07ec4c9c21f
Sergio Ivan Villamizar Alvarado - Ceiba Software <sergio.villamizar@ceiba.com.co>
2019-07-08 07:42:39 -0500

    cambios

--- src/main/java/co/com/ceiba/parqueadero/infraestructure/persistencia/repositorio/ReservaImpl.java
+++ src/main/java/co/com/ceiba/parqueadero/infraestructure/persistencia/repositorio/ReservaImpl.java
@@ -11,29 +11,25 @@ import co.com.ceiba.parqueadero.infraestructure.persistencia.exception.NotConten
 import co.com.ceiba.parqueadero.infraestructure.persistencia.exception.NotFoundException;
 import co.com.ceiba.parqueadero.infraestructure.persistencia.mapeador.ReservaMapper;
 
-
-
 @Repository
 @Transactional
-public class ReservaImpl implements ReservaRepositorio{
+public class ReservaImpl implements ReservaRepositorio {
 
 	private final ReservaJPA reservaJPA;
-	
+
 	public ReservaImpl(ReservaJPA reservaJPA) {
 		this.reservaJPA = reservaJPA;
 	}
 
 	@Override
 	public List<Reserva> listarReserva() {
-		List<Reserva> listReservas = this.reservaJPA.findAll().stream().map
-				                       (
-				                        ReservaMapper::toDomain
-				                       ).collect(Collectors.toList());
-		if(!listReservas.isEmpty()) {
+		List<Reserva> listReservas = this.reservaJPA.findAll().stream().map(ReservaMapper::toDomain)
+				.collect(Collectors.toList());
+		if (!listReservas.isEmpty()) {
 			return listReservas;
 		}
 		throw new NotContentException("No exiten Reservas registradas");
-	
+
 	}
 
 	@Override
@@ -43,44 +39,37 @@ public class ReservaImpl implements ReservaRepositorio{
 
 	@Override
 	public Reserva obtenerReservaPorId(long idReserva) {
-		
+
 		return ReservaMapper.toDomain(this.reservaJPA.findByIdReserva(idReserva)
-				             .orElseThrow(
-				            		 () -> new NotFoundException("La Reserva: " + 
-				                      idReserva + " no fue encontrada")));
-		
+				.orElseThrow(() -> new NotFoundException("La Reserva: " + idReserva + " no fue encontrada")));
+
 	}
 
 	@Override
 	public List<Reserva> listarReservaPendiente() {
-	
-		List<Reserva> listReservas = this.reservaJPA.findByFechaRetiro(null).stream().map
-				                          (
-				                          ReservaMapper::toDomain
-				                          ).collect(Collectors.toList());
-		if(!listReservas.isEmpty()) {
+
+		List<Reserva> listReservas = this.reservaJPA.findByFechaRetiro(null).stream().map(ReservaMapper::toDomain)
+				.collect(Collectors.toList());
+		if (!listReservas.isEmpty()) {
 			return listReservas;
 		}
 		throw new NotFoundException("No existen Reservas pendientes");
-		
+
 	}
 
 	@Override
 	public Reserva registrarSalida(Reserva reserva) {
-		return ReservaMapper.toDomain(reservaJPA.save(ReservaMapper.toEntity(reserva)));	
+		return ReservaMapper.toDomain(reservaJPA.save(ReservaMapper.toEntity(reserva)));
 	}
 
-	
-	//metodo creado
+	// metodo creado
 	@Override
 	public List<Reserva> reservasVehiculo(VehiculoEntity v) {
-		// TODO Auto-generated method stub
-		List<Reserva> listaReservaVehiculo=this.reservaJPA.findByVehiculo(v).stream().map
-				                                          (
-				                                        	ReservaMapper::toDomain	  
-				                                          ).collect(Collectors.toList());
+
+		return this.reservaJPA.findByVehiculo(v).stream().map(ReservaMapper::toDomain)
+				.collect(Collectors.toList());
+
 		
-		return listaReservaVehiculo;
 	}
 
 }
